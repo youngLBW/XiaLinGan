a03b393a23324b54100f70023e84a884520ff9fc
youngLBW <m13129903409@163.com>
2019-05-08 00:32:51 +0800

    增加了侧滑栏

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,6 +3,35 @@
 const app = getApp()
 
 Page({
+  data: {
+    open: false,
+    items: [
+      { name: 'USA', value: '美国' },
+      { name: 'CHN', value: '中国', checked: 'true' },
+      { name: 'BRA', value: '巴西' },
+      { name: 'JPN', value: '日本' },
+      { name: 'ENG', value: '英国' },
+      { name: 'TUR', value: '法国' },
+    ]
+  },
+
+  radioChange(e) {
+    console.log('radio发生change事件，携带value值为：', e.detail.value)
+  },
+
+  // 点击左上角小图标事件
+  tap_ch: function (e) {
+    if (this.data.open) {
+      this.setData({
+        open: false
+      });
+    } else {
+      this.setData({
+        open: true
+      });
+    }
+  },
+
   error(e) {
     console.log(e.detail)
   },
@@ -31,7 +60,7 @@ Page({
           title: '识别中',
         })
         wx.uploadFile({
-          url: 'http://192.168.0.242:5001',
+          url: 'https://fruits.beonel.club:9999',
           filePath: app.globalData.imgPath,
           name: 'image',
           success(res) {
